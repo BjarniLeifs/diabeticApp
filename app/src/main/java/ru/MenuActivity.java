b219ade7ea96b1eb0eff148e5e9818ee64981d86
snussurofa <annalaufey92@gmail.com>
2014-06-23 17:43:28 +0000

    let's try again

---
+++ app/src/main/java/ru/MenuActivity.java
@@ -0,0 +1,87 @@
+package ru;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import jBerry.MySugar.R;
+
+
+/**
+ * Created by Sindri on 19/06/14.
+ */
+
+
+public class MenuActivity extends ActionBarActivity {
+
+    protected final String TAG = "Debug";
+    LinearLayout background;
+    Button btn1, btn2, btn3, btn4, btn5, sendAdmin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+
+
+      /*  background =(LinearLayout) findViewById(R.id.background);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        sendAdmin = (Button) findViewById(R.id.sendAdmin);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //DoIt(v);
+            }
+        });
+    */
+
+        Log.d(TAG, "onCreate");
+    }
+
+
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected  void onRestart(){
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "OnPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+}
