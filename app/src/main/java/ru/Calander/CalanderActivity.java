0c85e3bbcadaecdba361dd394c94370952f6b1e2
krissi92 <jayflug@gmail.com>
2014-06-26 14:39:37 +0000

    just a class with folder and xml

---
+++ app/src/main/java/ru/Calander/CalanderActivity.java
@@ -0,0 +1,13 @@
+package ru.Calander;
+
+import android.os.Bundle;
+
+/**
+ * Created by Krissi on 26.6.2014.
+ */
+public class CalanderActivity {
+
+    public void onCreate(Bundle saveInstancesState){
+        
+    }
+}
