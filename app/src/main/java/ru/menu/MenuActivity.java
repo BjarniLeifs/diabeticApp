72d8c836a0c66b4d2d76e9131f2efd806699798c
Sindri55 <sindri12@ru.is>
2014-06-26 13:58:04 +0000

    Raw menu with a little function

--- app/src/main/java/ru/menu/MenuActivity.java
+++ app/src/main/java/ru/menu/MenuActivity.java
@@ -1,26 +1,40 @@
 package ru.menu;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import jBerry.MySugar.R;
 
-import static jBerry.MySugar.R.id.btn1;
 
-public class MenuActivity extends ActionBarActivity {
+public class MenuActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+    }
+
+    public void menuListener(View view){
+
+        switch (view.getId()){
+            case R.id.btn1:
+                //Intent intent = new Intent(this, SignupActivity.class);
+                //startActivity(intent);
+                break;
+            case R.id.btn2:
+
+                break;
+            case R.id.btn3:
+
+                break;
+
+            case R.id.btn4:
+                break;
+        }
     }
 
 
@@ -29,51 +43,5 @@ public class MenuActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /*
-        A placeholder fragment containing a some view
-     */
-
-    public static class PlaceholderFragment extends Fragment implements View.OnClickListener{
-        View rootView;
-
-        public PlaceholderFragment(){
-
-        }
-
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle saveInstanceState){
-            rootView = inflater.inflate(R.layout.activity_menu, container, false);
-
-            Button btn = (Button)rootView.findViewById(btn1);
-            btn.setOnClickListener(this);
-            return rootView;
-        }
-
-        @Override
-        public void onClick(View view) {
-
-            //Change views..
-
-            switch (view.getId())
-            {
-                case btn1:
-                    ((TextView)rootView.findViewById(R.id.myTextView)).setText("You clicked me!");
-                    break;
-            }
-        }
     }
 }
