125c85c976aad5f8257a7cd929db79775263fe97
Sindri55 <sindri12@ru.is>
2014-07-08 12:21:03 +0000

    Nada

--- app/src/main/java/ru/menu/StartActivity.java
+++ app/src/main/java/ru/menu/StartActivity.java
@@ -37,6 +37,9 @@ public class StartActivity extends Activity{
         list.add(new CustomClass(BitmapFactory.decodeResource(getResources(), R.drawable.diabetes),"Settings"));
         list.add(new CustomClass(BitmapFactory.decodeResource(getResources(), R.drawable.diabetes),"Stuff"));
 
+
+
+
         final CustomArrayAdapter adapter=new CustomArrayAdapter(getApplicationContext(), list);
 
         gridView.setAdapter(adapter);
