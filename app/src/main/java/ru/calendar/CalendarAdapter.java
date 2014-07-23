74fc6b6e4586412552b971f6fe66a1217dc58069
Sindri55 <sindri12@ru.is>
2014-07-23 14:22:19 +0000

    bb

--- app/src/main/java/ru/calendar/CalendarAdapter.java
+++ app/src/main/java/ru/calendar/CalendarAdapter.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class CalendarAdapter extends ArrayAdapter<CalanderMeal> {
 
 
-    private ArrayList<CalanderMeal> mCalList;
+    private ArrayList<CalendarMeal> mCalList;
 
     private CalendarService calService;
 
