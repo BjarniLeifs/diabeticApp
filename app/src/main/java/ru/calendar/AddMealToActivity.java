2f4924a3b5bf45c1f7630b028301bba893a10021
Sindri55 <sindri12@ru.is>
2014-08-23 04:02:15 +0000

    tenging

--- app/src/main/java/ru/calendar/AddMealToActivity.java
+++ app/src/main/java/ru/calendar/AddMealToActivity.java
@@ -3,16 +3,26 @@ package ru.calendar;
 import android.app.Activity;
 import android.content.Intent;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 
+import com.jberry.dto.CalanderMeal;
+import com.jberry.dto.FoodTO;
+import com.jberry.services.calendar.CalendarService;
+import com.jberry.services.calendar.CalendarServiceFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
@@ -46,15 +56,19 @@ public class AddMealToActivity extends Activity {
             @Override
             public void onClick(View v) {
 
+
+                EditText Name = (EditText) findViewById(R.id.nameid);
+                String mealName = Name.getText().toString();
+
                 cal.setTimeInMillis(calendarView.getDate());
                 Date date = new Date(calendarView.getDate() / 1000L);
-
                 long hour = timePicker.getCurrentHour();
                 long min = timePicker.getCurrentMinute();
 
                 hour = TimeUnit.HOURS.toMillis(hour);
                 min = TimeUnit.MINUTES.toMillis(min);
 
+                // Fæ ranga niðurstöðu en Date gefur rétt frá sér.
                 timestamp = date.getTime()+hour+min;
 
                 for (int i = 0; i < Events.idItems.length; i++) {
@@ -66,10 +80,54 @@ public class AddMealToActivity extends Activity {
                     }
                 }
 
+                MyTaskParams params = new MyTaskParams(mealName, timestamp);
+                saveMealToCalander saveIt = new saveMealToCalander();
+                saveIt.execute(params);
+
                 //AddToCalendarAdapter.addMeal(data, timestamp, hour, min);
                 Intent intent = new Intent(AddMealToActivity.this, CalendarActivity.class);
                 startActivity(intent);
             }
         });
     }
+
+    private static class MyTaskParams {
+
+
+        String mealName;
+        long timeStamp;
+
+        MyTaskParams(String mealName, long timeStamp) {
+
+
+            this.timeStamp = timeStamp;
+            this.mealName = mealName;
+        }
+    }
+
+    private class saveMealToCalander extends AsyncTask<MyTaskParams, Boolean, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(MyTaskParams... params) {
+
+            MyTaskParams meal = params[0];
+            String mealName = meal.mealName;
+            long TimeStamp = meal.timeStamp;
+
+            CalendarService service = CalendarServiceFactory.getCalanderService();
+            try {
+                return service.saveMealToCalander(mealName, TimeStamp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean i) {
+            Toast.makeText(getBaseContext(), "Bætt við í dagatal!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
 }
+
